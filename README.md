a051eefd765d99585c6a071b5c8bd0a2856a91a5
Gionatan Lombardi <gionatan@vangogh-creative.it>
2016-03-07 16:54:46 +0100

    Complete Plugin Refatoring: removed jQuery requirement

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # tabs-manager-light
-A lightweight JS script for tab management. Requires jQuery.
+A lightweight JS script for tab management. 
+
+**Version 2.0.0**
+jQuery requirements removed.
 
 Based on Remy Sharp's post, ["How tabs should work"](https://24ways.org/2015/how-tabs-should-work/)
+
+## Borwsers support
+IE9+
--- src/tabs-manager-light.js
+++ src/tabs-manager-light.js
@@ -1,111 +1,191 @@
 /*!
  * tabs-manager-light 
- * v1.0.0
+ * v2.0.0
  * (https://github.com/gionatan-lombardi/tabs-manager-light)
  * Based on Remy Sharp's post, "How tabs should work" - https://24ways.org/2015/how-tabs-should-work/
  * Copyright (c) Remy Sharp - http://remysharp.com
  */
 
-if (typeof jQuery === 'undefined') {
-  throw new Error('Tabs Manager Light requires jQuery')
+( function(window) {
+
+'use strict';
+   
+// Utility Functions
+
+//http://youmightnotneedjquery.com/#deep_extend
+function extend(out) {
+  out = out || {};
+
+  for (var i = 1; i < arguments.length; i++) {
+    var obj = arguments[i];
+
+    if (!obj)
+      continue;
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object')
+          out[key] = extend(out[key], obj[key]);
+        else
+          out[key] = obj[key];
+      }
+    }
+  }
+
+  return out;
 }
 
-(function($) {
+// http://youmightnotneedjquery.com/#add_class
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className);
+  else
+    el.className += ' ' + className;
+}
 
-  'use strict';
+// http://youmightnotneedjquery.com/#remove_class
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
 
-  $.fn.tabsManagerLight = function( options ) {
-   
+var buildObj = {
+
+  // a temp value to cache *what* we're about to show
+  target: null,
+
+  // Plugin Specific Functions
+
+  update: function update() {
     var self = this;
 
-    // Extend the default options with those provided.
-    self.opts = $.extend( {}, $.fn.tabsManagerLight.defaults, options );
+    if (self.target) {
+      self.target.setAttribute('id', self.target.dataset.old);
+      self.target = null;
+    }
 
-    this.each( function () {
+    var hash = window.location.hash;
+    if (self.targets.indexOf(hash) !== -1) {
+      return self.show(hash);
+    }
 
-      // Tabs jQuery collection
-      var $tabs = $(this).find('a[href^="#"]');
+    // Fixes going "back" on the browser nav to an empty state
+    if (!hash) {
+      self.show();
+    }
+  },
 
-      // a temp value to cache *what* we're about to show
-      var target = null;
+  show: function show(id) {
+    var self = this;
+    
+    // The current Tab
+    var currTab; 
 
-      // collect all the tabs
-      var tabs = $tabs.on('click', function() {
-        console.log('click')
-        target = $(this.hash).removeAttr('id');
+    // if no value was given, let's take the first panel
+    if (!id) {
+      id = self.targets[0];
+    }
+
+    // Loops over the tabs
+    for (var i = 0; i < self.allTabs.length; ++i) {
+      var el = self.allTabs[i];
+
+      // remove the selected class from the tabs
+      removeClass(el, self.options.tabCurrentClass);
+      el.setAttribute('aria-selected', 'false');
+      
+      // and add it to the one the user selected
+      if (el.hash === id) {
+        addClass(el, self.options.tabCurrentClass);
+        el.setAttribute('aria-selected', 'true');
+      } 
+    }
+    
+    // Loops over the panels
+    for (var i = 0; i < self.panels.length; ++i) {
+      var el = self.panels[i];
+
+      // Remove open class to all the panels
+      removeClass(el, self.options.panelOpenClass);
+      el.setAttribute('aria-hidden', 'false');
+      
+      // adds open class the the target panel
+      if (('#'+el.id) === id) {
+        addClass(el, self.options.panelOpenClass);
+        el.setAttribute('aria-hidden', 'true');
+      } 
+    }
+  },
+
+  init: function init(element) {
+    var self = this;
+
+    self.targets = []
+
+    // Collects all the tabs in a NodeList
+    self.allTabs = document.querySelectorAll(element);
+
+    // Loops over the tab's NodeList
+    for (var i = 0; i < self.allTabs.length; ++i) {
+      var el = self.allTabs[i];
+      el.addEventListener('click', function() {
+        self.target = document.querySelector(this.hash);
+        self.target.removeAttribute("id");
         if (location.hash === this.hash) {
-          setTimeout(update);
+          setTimeout(self.update);
         }
-      }).attr('tabindex', '0');
-
-      // get an array of the panel ids (from the anchor hash)
-      var targets = tabs.map(function() {
-        return this.hash;
-      }).get();
-
-      // use those ids to get a jQuery collection of panels
-      var panels = $(targets.join(',')).each(function() {
-        // keep a copy of what the original el.id was
-        $(this).data('old-id', this.id);
       });
+      // get an array of the panel ids (from the anchor hash)
+      el.setAttribute('tabindex', 0);
+      // populates the array of the panel ids (from the anchor hash)
+      self.targets.push(el.hash);
+    }
 
-      function update() {
-        console.log('update')
-        if (target) {
-          target.attr('id', target.data('old-id'));
-          target = null;
-        }
+    // use those ids to get a NodeList collection of panels
+    self.panels = document.querySelectorAll(self.targets.join(','));
 
-        var hash = window.location.hash;
-        if (targets.indexOf(hash) !== -1) {
-          return show(hash);
-        }
+    // Loops over the panel's NodeList to set the data-old attribute
+    for (var i = 0; i < self.panels.length; ++i) {
+      var el = self.panels[i];
+      el.dataset.old = el.id;
+    }
 
-        // Fixes going "back" on the browser nav to an empty state
-        if (!hash) {
-          show();
-        }
-      }
+    window.addEventListener( 'hashchange', self.update.bind(self) );
 
-      function show(id) {
-        // if no value was given, let's take the first panel
-        if (!id) {
-          id = targets[0];
-        }
-        // remove the selected class from the tabs,
-        // and add it back to the one the user selected
-        tabs.removeClass(self.opts.tabCurrentClass).attr('aria-selected', 'false').filter(function() {
-          return (this.hash === id);
-        }).addClass(self.opts.tabCurrentClass).attr('aria-selected', 'true');
+    // initialise
+    if (self.targets.indexOf(window.location.hash) !== -1) {
+      self.update();
+    } else {
+      self.show();
+    }
+  },
 
-        // now hide all the panels, then filter to
-        // the one we're interested in, and show it
-        panels.removeClass(self.opts.panelOpenClass).attr('aria-hidden', 'true').filter(id).addClass(self.opts.panelOpenClass).attr('aria-hidden', 'false');
-      }
+};
 
-      // Public function
-      function init() {
-
-        window.addEventListener('hashchange', update);
-
-        // initialise
-        if (targets.indexOf(window.location.hash) !== -1) {
-          update();
-        } else {
-          show();
-        }
-      };
-
-      init();
-
-    });
-
-  };
-
-  // Default Plugin Options
-  $.fn.tabsManagerLight.defaults = {
+// The Plugin Exposed Function (init)
+function tabsManagerLight(element, cstOptions) {
+  var defaultOptions = {
     tabCurrentClass: 'is-current',
     panelOpenClass: 'is-open'
-  };
+  }
+  var options = extend(defaultOptions, cstOptions);
+  var o = Object.create(buildObj);
+  o.options = options;
+  o.init(element);
+};
 
-})(jQuery);
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( tabsManagerLight );
+} else if ( typeof exports === 'object' ) {
+  // CommonJS
+  module.exports = tabsManagerLight;
+} else {
+  // browser global
+  window.tabsManagerLight = tabsManagerLight;
+}
+
+})( window );
